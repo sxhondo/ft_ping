4b3eb699eb03ddd76636dcad92931bf2e080fee1
sxhondo <w13cho@gmail.com>
2023-03-03 21:36:29 +0300

    +outpackhdr/icmp/icmhdr

--- src/example.c
+++ src/example.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
@@ -20,15 +21,15 @@ int main(int argc, char **argv)
     struct addrinfo *res;
 
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET; // AF_UNSPEC for any
-    hints.ai_socktype = SOCK_DGRAM; // SOCK_DGRAM UDP, SOCK_STREAM TCP, SOCK_RAW - ICMP and others protocols 
+    hints.ai_family = AF_INET;      // AF_UNSPEC for any
+    hints.ai_socktype = SOCK_DGRAM; // SOCK_DGRAM UDP, SOCK_STREAM TCP, SOCK_RAW - ICMP and others protocols
     // hints.ai_flags = AI_PASSIVE; // use my ip (insted pass first paremeter to getaddrinfo)
 
-    /* 
+    /*
         int
         getaddrinfo(const char *hostname, const char *servname, const struct addrinfo *hints, struct addrinfo **res);
 
-        The getaddrinfo() function is used to get a list of IP addresses and port numbers for host hostname and service servname. 
+        The getaddrinfo() function is used to get a list of IP addresses and port numbers for host hostname and service servname.
         It is a replacement for and provides more flexibility than the gethostbyname(3) and getservbyname(3) functions.
         NULL - could be address specified by user
         hints - hints filled above and as for man() "hints is an optional pointer to a struct addrinfo, as defined by ⟨netdb.h⟩"
@@ -51,8 +52,8 @@ int main(int argc, char **argv)
             struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
             addr = &(ipv4->sin_addr);
             ipver = "IPv4";
-        } 
-        else 
+        }
+        else
         {
             struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
             addr = &(ipv6->sin6_addr);
--- src/ft_ping.c
+++ src/ft_ping.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netinet/ip_icmp.h>
 
 // struct ping_packet
 // {
@@ -27,6 +29,7 @@ main(int argc, char **argv)
     if (argc != 2)
     {
         printf("%s\n", "usage: ");
+        exit(0);
     }
 
     int status;
@@ -37,41 +40,56 @@ main(int argc, char **argv)
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_RAW;
 
-    printf("performing DNS lookup for %s:\n\n", argv[1]);
     if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0)
     {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(2);
     }
 
-    struct sockaddr_in *dest_sock_in = NULL;
+    char ipstr[INET6_ADDRSTRLEN];
+    struct sockaddr_in *dest = NULL;
     for (struct addrinfo *p = res; p != NULL; p = p->ai_next)
     {
         if (p->ai_family == AF_INET)
         {
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            char ipstr[INET6_ADDRSTRLEN];
-            inet_ntop(p->ai_family, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
-            printf("using ip: %s\n", ipstr);
+            dest = (struct sockaddr_in *)p->ai_addr;
+            inet_ntop(p->ai_family, &(dest->sin_addr), ipstr, sizeof(ipstr));
             break ;
         }
     }
 
-    // int icmp_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    // if (icmp_sock < 0)
-    // {
-    //     perror("ping: icmp open socket");
-    //     exit(2);
-    // }
+    printf("PING %s (%s) %d(%d) bytes of data.\n", argv[1], ipstr, 1, 1);
+    int icmp_sock = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (icmp_sock < 0)
+    {
+        perror("ping: icmp open socket");
+        exit(2);
+    }
 
-    // int sent = sendto(
-    //     icmp_sock,
-    //     &packet,
-    //     sizeof(packet),
-    //     0,
-    //     (struct sockaddr *)sock_in,
-    //     sizeof(*sock_in)
-    // );
+    u_char outpackhdr[IP_MAXPACKET], *outpack;
+    outpack = outpackhdr + sizeof(struct ip);
 
+    struct icmp *icp;
+
+    icp = (struct icmp *)outpack;
+	icp->icmp_type = ICMP_ECHO;
+	icp->icmp_code = 0;
+	icp->icmp_cksum = 0;
+	icp->icmp_seq = htons(1);
+	icp->icmp_id = getpid();
+
+    int cc = ICMP_MINLEN + 56;
+
+    int sent = sendto(
+        icmp_sock,
+        &outpack,
+        cc,
+        0,
+        (struct sockaddr *)dest,
+        sizeof(dest)
+    );
+
+    freeaddrinfo(res);
+    close(icmp_sock);
     return 0;
 }
